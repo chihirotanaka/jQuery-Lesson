fec62a4be53534e27350e0bef0f46a76f7d7b17c
chihirotanaka <61111335+chihirotanaka@users.noreply.github.com>
2020-03-31 00:11:12 +0900

    [start6-8]

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 $(function(){
-	$('.box1').on('click',function(){
-		$('.box1').addClass('box1-ext');
-	});
-// マウスを乗せたら青
+// 	$('.bg1').on('click', function(){
+// 	  $('.bg1').slideUp();
+//   });
 
+// 	$('.bg2').on('click', function(){
+// 	  $('.bg2').slideUp();
+//   });
 
-	$('.box1').mouseout(function(){
-		$('.box1').removeClass('box1-ext');
-// マウスを外したら赤
-	});
+// 	$('.bg3').on('click', function(){
+// 	  $('.bg3').slideUp();
+//   });
+
+// 	$('.bg4').on('click', function(){
+// 	  $('.bg4').slideUp();
+//   });
+// });
+  $('button').on('click',function(){
+  	$('ul').children().css('color','red');
+  });
 });
+
